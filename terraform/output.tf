d663e1bcf2a729a2a84900a7e21f801ec8e0fe13
xyhhx <xyhhx@disr.it>
2023-02-24 23:13:10 -0500

    update output to capture nodes' nics' macs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,19 @@
+output "control_plane_macs" {
+  value = {
+    for k, v in proxmox_vm_qemu.talos-control-plane : k => flatten([
+      for nics in v.network : [
+        nics.macaddr
+      ]
+    ])
+  }
+}
 
+output "worker_macs" {
+  value = {
+    for k, v in proxmox_vm_qemu.talos-worker : k => flatten([
+      for nics in v.network : [
+        nics.macaddr
+      ]
+    ])
+  }
+}
